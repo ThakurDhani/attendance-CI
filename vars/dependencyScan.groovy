e3514407d5afff7254189d81fb1108ec89e46b4e
ThakurDhani <thakurdhani143@gmail.com>
2025-12-18 15:06:10 +0530

    Update dependencyScan.groovy

--- vars/dependencyScan.groovy
+++ vars/dependencyScan.groovy
@@ -1,7 +1,14 @@
 def call() {
+
     sh '''
+        set +e
+
         echo "Running Dependency Scan..."
-        pip install pip-audit
+
+        export PATH=$HOME/.local/bin:$PATH
+
+        pip install pip-audit >/dev/null 2>&1 || true
+
         pip-audit || true
     '''
 }
