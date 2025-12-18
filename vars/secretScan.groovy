e08e21938cf834756d50a0c47a99575e5914871b
ThakurDhani <thakurdhani143@gmail.com>
2025-12-18 15:01:40 +0530

    Update secretScan.groovy

--- vars/secretScan.groovy
+++ vars/secretScan.groovy
@@ -3,12 +3,29 @@ def call() {
     sh '''
         echo "Running Gitleaks..."
 
+        # Install dependencies
+        sudo apt-get update -y
+        sudo apt-get install -y wget unzip
+
+        # Install Gitleaks if missing
         if ! command -v gitleaks &> /dev/null
         then
-            echo "Installing Gitleaks..."
-            curl -sSfL https://raw.githubusercontent.com/gitleaks/gitleaks/master/install.sh | bash
+            echo "Downloading Gitleaks latest release..."
+            wget -q https://github.com/gitleaks/gitleaks/releases/latest/download/gitleaks-linux-amd64.zip
+
+            echo "Unzipping..."
+            unzip -o gitleaks-linux-amd64.zip
+
+            echo "Moving binary..."
+            sudo mv gitleaks /usr/local/bin/
+
+            echo "Making binary executable"
+            sudo chmod +x /usr/local/bin/gitleaks
+
+            gitleaks version
         fi
 
+        echo "Running scan..."
         gitleaks detect \
         --source . \
         --report-format json \
@@ -16,5 +33,5 @@ def call() {
         || true
     '''
 
-    archiveArtifacts artifacts: 'gitleaks-report.json'
+    archiveArtifacts artifacts: 'gitleaks-report.json', allowEmptyArchive: true
 }
